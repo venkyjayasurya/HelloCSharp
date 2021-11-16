927fd2c7c4287ca3b50561f1dd596ca3cb364c4d
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-11-16 23:37:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+            Console.WriteLine("Hello C Sharp");
+            Console.WriteLine(number);
+        }
+    }
+}
