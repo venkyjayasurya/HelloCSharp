3b2b52b8932ff4e2df5dddd81e21666b4d6c9751
Venkateshwara Rao Jayasurya <venkyjayasurya@gmail.com>
2021-11-16 23:43:13 +0530

    datatypes

--- Program.cs
+++ Program.cs
@@ -9,6 +9,26 @@ namespace HelloCSharp
             int number = 1;
             Console.WriteLine("Hello C Sharp");
             Console.WriteLine(number);
+
+            //datatypes
+            byte num=2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Venkatesh";
+            bool isWorking = false;
+
+            //for dynamic datatype allocation
+            var randomDatatype = "Jayasurya";
+
+            Console.WriteLine(num);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            Console.WriteLine(randomDatatype);
         }
     }
 }
